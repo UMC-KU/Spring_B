7981d99a7824976bcf260380620b521438d50e1c
KangSungBum <peter9706@naver.com>
2022-05-20 15:01:10 +0900

    Initial Commit

---
+++ sungbum/src/main/java/com/example/demo/src/post/PostController.java
@@ -0,0 +1,102 @@
+package com.example.demo.src.post;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.post.model.GetPostsRes;
+import com.example.demo.src.post.model.PatchPostsReq;
+import com.example.demo.src.post.model.PostPostsReq;
+import com.example.demo.src.post.model.PostPostsRes;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final PostProvider postProvider;
+    @Autowired
+    private final PostService postService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+    public PostController(PostProvider postProvider, PostService postService, JwtService jwtService){
+        this.postProvider = postProvider;
+        this.postService = postService;
+        this.jwtService = jwtService;
+    }
+
+    @ResponseBody
+    @GetMapping("") // (GET) 127.0.0.1:9000/users
+    public BaseResponse<List<GetPostsRes>> getPosts(@RequestParam int userIdx) {
+        try{
+            List<GetPostsRes> getPostsRes = postProvider.retrievePosts(userIdx);
+            return new BaseResponse<>(getPostsRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("") // (POST) 127.0.0.1:9000/users
+    //post는 body 필요
+    public BaseResponse<PostPostsRes> createPosts(@RequestBody PostPostsReq postPostsReq) {
+        try{
+            if (postPostsReq.getContent().length() > 450) {
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_INVALID_CONTENTS);
+            }
+
+            if (postPostsReq.getPostImgUrls().size() < 1) {
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_EMPTY_IMGURL);
+            }
+
+            PostPostsRes postPostsRes = postService.createPosts(postPostsReq.getUserIdx(), postPostsReq);
+
+            return new BaseResponse<>(postPostsRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+
+    @ResponseBody
+    @PatchMapping("/{postIdx}") // (PATCH) 127.0.0.1:9000/users
+    //post는 body 필요
+    public BaseResponse<String> modifyPost(@PathVariable ("postIdx") int postIdx, @RequestBody PatchPostsReq patchPostsReq) {
+        try{
+            if (patchPostsReq.getContent().length() > 450) {
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_INVALID_CONTENTS);
+            }
+
+            postService.modifyPost(patchPostsReq.getUserIdx(), postIdx, patchPostsReq);
+
+            String result = "게시물 정보 수정을 완료하였습니다";
+            return new BaseResponse<>(result);
+
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/{postIdx}/status") // (PATCH) 127.0.0.1:9000/users
+    //post는 body 필요
+    public BaseResponse<String> deletePost(@PathVariable ("postIdx") int postIdx) {
+        try{
+            postService.deletePost(postIdx);
+            String result = "게시물 정보 삭제를 완료하였습니다";
+            return new BaseResponse<>(result);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+}
---
+++ sungbum/src/main/java/com/example/demo/src/post/model/GetPostImgRes.java
@@ -0,0 +1,16 @@
+package com.example.demo.src.post.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetPostImgRes {
+
+    private int postImgIdx;
+    private String postImgUrl;
+
+}
